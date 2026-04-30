451bdd3703cc2bb6f0555979debb3bd56e36ead8
Amir_Ashour <amirdawod19@gmail.com>
2026-04-30 11:35:47 +0300

    Refactor validation functions with type hints

--- task-16     22-4-2026/template/core/validation.php
+++ task-16     22-4-2026/template/core/validation.php
@@ -1,113 +1,25 @@
+
+
 <?php
 
 
-function validateRequired($value, $fieldName)
+function validateRequired( string  $value, string $fieldName)
 {
     return empty($value) ? "$fieldName is required" : null;
 }
 
 
 
-function validateName($name)
-{
-    return  preg_match("/^[a-zA-Z ]+$/", $name) ? null : 'invalid Name';
-}
 
-function validatePrice($price)
+
+function validatePrice(int|float $price) :int|float
 {
     return  is_numeric($price) ? null : 'invalid Price';
 }
 
 
-function validateEmail($email)
-{
-    return  filter_var($email, FILTER_VALIDATE_EMAIL) ? null : "invalid Email";
-}
 
-
-
-function validatePhone($phone)
-{
-    return  preg_match("/^\+[0-9 ( ) -]+$/", $phone)  ? null : " invalid Phone ";
-}
-
-
-
-function validatePassword($password)
-{
-    if (!preg_match("/[A-Z]/", $password)) {
-        return "password must be contains char captial";
-    }
-    if (!preg_match("/[a-z]/", $password)) {
-        return "password must be contains char small";
-    }
-    if (!strlen($password) > 6) {
-        return "password must be not  lower than 6";
-    }
-}
-
-
-
-function checkPassword($password, $confirm_password)
-{
-    if ($password != $confirm_password) {
-        return "password is match ";
-    }
-}
-
-
-function validateRegister($name, $email, $phone, $password, $confirm_password)
-{
-
-    $fields = [
-        "name" => $name,
-        "email" => $email,
-        "phone" => $phone,
-        "password" => $password,
-        "confirm_password" => $confirm_password
-    ];
-    foreach ($fields as $fieldName => $value) {
-        if ($error = validateRequired($value, $fieldName)) {
-            return $error;
-        }
-    }
-    if ($error = validateName($name)) {
-        return $error;
-    }
-
-    if ($error = validateEmail($email)) {
-        return $error;
-    }
-
-    if ($error = validatePhone($phone)) {
-        return $error;
-    }
-
-    if ($error = validatePassword($password)) {
-        return $error;
-    }
-
-    if ($error = checkPassword($password, $confirm_password)) {
-        return $error;
-    }
-}
-
-
-function validateLogin($email, $password)
-{
-
-    $fields = [
-        "email" => $email,
-        "password" => $password
-    ];
-    foreach ($fields as $fieldName => $value) {
-        if ($error = validateRequired($value, $fieldName)) {
-            return $error;
-        }
-    }
-}
-
-function validateImage($imageExtension, $imageSize, $imageTmpName)
+function validateImage(string $imageExtension,int  $imageSize,string $imageTmpName)
 {
 
     $allowedExtensions = ['png', 'jpg', 'jpeg', 'svg'];
@@ -127,7 +39,7 @@ function validateImage($imageExtension, $imageSize, $imageTmpName)
     }
 }
 
-function validateProduct($name, $price, $imageName, $description, $type, $imageTmpName, $imageExtension, $imageSize)
+function validateProduct(string $name, int $price, string $imageName,string $description, string $type,string $imageTmpName,string $imageExtension, int $imageSize)
 {
     $fields = [
         "name" => $name,
@@ -149,29 +61,3 @@ function validateProduct($name, $price, $imageName, $description, $type, $imageT
 }
 
 
-function validateContact($name, $email, $phone, $message)
-{
-
-    $fields = [
-        "name" => $name,
-        "email" => $email,
-        "phone" => $phone,
-        "messswage" => $message
-    ];
-    foreach ($fields as $fieldName => $value) {
-        if ($error = validateRequired($value, $fieldName)) {
-            return $error;
-        }
-    }
-    if ($error = validateName($name)) {
-        return $error;
-    }
-
-    if ($error = validateEmail($email)) {
-        return $error;
-    }
-
-    if ($error = validatePhone($phone)) {
-        return $error;
-    }
-}
